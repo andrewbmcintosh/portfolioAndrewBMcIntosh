bb28d176856dd272fc84932213834ea6c8a1dae4
andrewmcintosh <abmcintosh7@gmail.com>
2019-03-08 15:40:52 -0500

    styling now with grid

--- final-portfolio-andrew-b-mcintosh/src/components/landingSection/LandingSection.js
+++ final-portfolio-andrew-b-mcintosh/src/components/landingSection/LandingSection.js
@@ -6,12 +6,17 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
-const StyledJumbotron = styled(Jumbotron)`
+const StyledJumbotron = styled.div`
+  grid-column: 1/13;
+  grid-row: 1;
   background-color: red;
-  height: 400px;
 `;
 
 const LandingDiv = styled.div`
+  display: grid;
+  grid-template-columns: repeat(12, 1fr);
+  grid-template-rows: 22% 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
+
   background-color: #91b7bd;
   height: 1464px;
 `;
